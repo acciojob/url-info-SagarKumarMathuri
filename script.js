59c71a2e40d18ec3a2dc42b10670480a7b054f79
sagar kumar mathuri <sagarku2002@gmail.com>
2026-04-25 10:53:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// Get the current URL
+let currentURL = window.location.href;
+
+// Find the length of the URL
+let lengthOfURL = currentURL.length;
+
+// Display the result
+alert("The length of the URL is: " + lengthOfURL);
